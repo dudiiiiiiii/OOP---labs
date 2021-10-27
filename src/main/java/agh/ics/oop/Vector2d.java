4d9af6f9be3110b570afd02bea51955722599cbf
dudiiiiiiii <79313233+dudiiiiiiii@users.noreply.github.com>
2021-10-27 21:32:37 +0200

    Update Vector2d.java

--- src/main/java/agh/ics/oop/Vector2d.java
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -1,45 +1,45 @@
-package agh.ics.oop;
+// package agh.ics.oop;
 
-public class Vector2d {
-    public int x;
-    public int y;
+// public class Vector2d {
+//     public int x;
+//     public int y;
 
-    public Vector2d(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
+//     public Vector2d(int x, int y){
+//         this.x = x;
+//         this.y = y;
+//     }
 
-    public String toString(){
-        return "("+Integer.toString(this.x)+","+Integer.toString(this.y)+")";
-    }
+//     public String toString(){
+//         return "("+Integer.toString(this.x)+","+Integer.toString(this.y)+")";
+//     }
 
-    public boolean precedes(Vector2d other){
-        if(this.x <= other.x && this.y <= other.y) {
-            return true;
-        }
-        return false;
-    }
+//     public boolean precedes(Vector2d other){
+//         if(this.x <= other.x && this.y <= other.y) {
+//             return true;
+//         }
+//         return false;
+//     }
 
-    public boolean follows(Vector2d other){
-        if(this.x >= other.x && this.y >= other.y) {
-            return true;
-        }
-        return false;
-    }
+//     public boolean follows(Vector2d other){
+//         if(this.x >= other.x && this.y >= other.y) {
+//             return true;
+//         }
+//         return false;
+//     }
 
-    public Vector2d upperRight(Vector2d other) {
-        if(this.x >= other.x) {
-            int x = this.x;
-        }
-        else{
-            int x = other.x;
-        }
-        if(this.y >= other.y) {
-            int y = this.y;
-        }
-        else{
-            int y = other.y;
-        }
-        return Vector2d(x, y);
-    }
-}
+//     public Vector2d upperRight(Vector2d other) {
+//         if(this.x >= other.x) {
+//             int x = this.x;
+//         }
+//         else{
+//             int x = other.x;
+//         }
+//         if(this.y >= other.y) {
+//             int y = this.y;
+//         }
+//         else{
+//             int y = other.y;
+//         }
+//         return Vector2d(x, y);
+//     }
+// }
